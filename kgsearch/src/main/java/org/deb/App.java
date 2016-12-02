d30b7accdcafbaf641b9854265cc9a7c521dfdac
debmalyajash <debmalya.jash@gmail.com>
2016-12-02 13:56:56 +0800

    Added limit, so now while searching user can set the limit to search result. Permissable range is 1 - 10

--- kgsearch/src/main/java/org/deb/App.java
+++ kgsearch/src/main/java/org/deb/App.java
@@ -39,8 +39,23 @@ public class App {
 			JSONParser parser = new JSONParser();
 
 			App app = new App();
-			Response response = app.entitySearch(in, requestFactory, parser, properties);
+			int limit = 1;
+			try {
+				if (args.length == 2) {
+					limit = Integer.parseInt(args[1]);
+					if (limit > 10) {
+						limit = 10;
+					} else if (limit < 0) {
+						limit = 1;
+					}
+				}
+
+			} catch (NumberFormatException neverMind) {
+
+			}
+			Response response = app.entitySearch(in, requestFactory, parser, properties, limit);
 			System.out.println(response);
+			System.out.println("Total number of elements :" + response.getResultList().size());
 
 		} catch (FileNotFoundException e) {
 			System.err.println(
@@ -63,8 +78,29 @@ public class App {
 
 	}
 
+	/**
+	 * 
+	 * @param in
+	 *            - InputStream to accept the parameters.
+	 * @param requestFactory
+	 *            - HttpRequestFactory to make the request.
+	 * @param parser
+	 *            - To parse JSON response.
+	 * @param properties
+	 *            - To get the API_KEY
+	 * @param limit
+	 *            - How many result will be listed (Range is 1 - 10)
+	 * @param entities
+	 *            - What entities will be searched.
+	 * @return Search response.
+	 * @throws IOException
+	 * @throws ParseException
+	 *             - If there is any error while parsing JSON response.
+	 */
 	public Response entitySearch(Scanner in, HttpRequestFactory requestFactory, JSONParser parser,
-			Properties properties, String... entities) throws IOException, ParseException {
+			Properties properties, int limit, String... entities) throws IOException, ParseException {
+		System.out.println("Default Search limit :" + limit);
+		int acceptedLimit = limit;
 		String apiKey = properties.getProperty("API_KEY");
 		Response kgSearchResponse = new Response();
 		List<String> responseList = new ArrayList<>();
@@ -81,21 +117,30 @@ public class App {
 			GenericUrl url = new GenericUrl("https://kgsearch.googleapis.com/v1/entities:search");
 
 			if (entities == null || entities.length == 0) {
-				System.out.println("Enter entity name :");
+				System.out.println("Please enter entity name :");
 				entities = new String[] { in.nextLine() };
+				System.out.println("Please enter search limit (1 - 10):");
+				try {
+					acceptedLimit = Integer.parseInt(in.nextLine());
+					if (acceptedLimit < -1 || acceptedLimit > 10) {
+						acceptedLimit = limit;
+					}
+				} catch (NumberFormatException neverMind) {
+
+				}
 				System.out.println("Please wait, searching ...");
 			}
 
 			for (String eachEntity : entities) {
 				url.put("query", eachEntity);
-				url.put("limit", "10");
+				url.put("limit", acceptedLimit);
 				url.put("indent", "true");
 				url.put("key", apiKey);
 				HttpRequest request = requestFactory.buildGetRequest(url);
 				HttpResponse httpResponse = request.execute();
 				JSONObject response = (JSONObject) parser.parse(httpResponse.parseAsString());
 				JSONArray elements = (JSONArray) response.get("itemListElement");
-				 
+
 				if (elements != null && !elements.isEmpty()) {
 					for (Object element : elements) {
 
@@ -103,17 +148,18 @@ public class App {
 							responseList
 									.add(JsonPath.read(element, "$.result.detailedDescription.articleBody").toString());
 							urlList.add(JsonPath.read(element, "$.result.detailedDescription.url").toString());
-//							imageList.add(JsonPath.read(element, "$.result.image.url").toString());
+							// imageList.add(JsonPath.read(element,
+							// "$.result.image.url").toString());
 						} catch (Throwable ignore) {
 						}
 
 					}
 				} else {
 					String[] allEntities = eachEntity.split(" ");
-					if (allEntities.length > 0){
-						return entitySearch(in,requestFactory,parser,properties,allEntities);
+					if (allEntities.length > 0) {
+						return entitySearch(in, requestFactory, parser, properties, limit, allEntities);
 					} else {
-					responseList.add("'" + eachEntity + "' not found in the knowledge graph.");
+						responseList.add("'" + eachEntity + "' not found in the knowledge graph.");
 					}
 				}
 			}
