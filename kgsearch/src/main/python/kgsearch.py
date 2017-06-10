9d6c4484102d7d23902241fea02341c9f5ec5841
debmalyajash <debmalya.jash@gmail.com>
2017-06-10 15:40:56 +0800

    Modified python search example

--- kgsearch/src/main/python/kgsearch.py
+++ kgsearch/src/main/python/kgsearch.py
@@ -7,11 +7,13 @@ query = raw_input( 'What to search :')
 service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
 params = {
     'query': query,
-    'limit': 10,
+    'limit': 100,
     'indent': True,
     'key': api_key,
 }
 url = service_url + '?' + urllib.urlencode(params)
 response = json.loads(urllib.urlopen(url).read())
 for element in response['itemListElement']:
-  print element['result']['name'] + ' (' + str(element['resultScore']) + ')'
+  if 'name' in element['result'] and 'description' in element['result']:
+#      print element['result']['name'] + ' (' + str(element['resultScore']) + ')'
+      print element['result']['name'] + ' ' + element['result']['description']
